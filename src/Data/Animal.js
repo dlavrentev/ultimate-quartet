369fb68f997ef903140a856341b30559a9677738
dlavrentev <dimitrilavrentev96@gmail.com>
2022-06-20 09:55:48 +0200

    Create Animal.js

---
+++ src/Data/Animal.js
@@ -0,0 +1,13 @@
+
+
+export default class Animal {
+    constructor(name, image, size, weight, age, offspring, speed) {
+        this.name = name;
+        this.image = iamge;
+        this.size = size;
+        this.weight = weight;
+        this.age = age;
+        this.offspring = offspring;
+        this.speed = speed;
+    }
+}
