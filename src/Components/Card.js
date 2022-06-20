2e349c02eb0c0d0179283b2c601829649d7dc0eb
dlavrentev <dimitrilavrentev96@gmail.com>
2022-06-20 10:24:36 +0200

    Update Card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,22 +4,33 @@ import Animal from '../Data/Animal.js'
 
 export default function Card() {
 
-  const elephant = new Animal('Elefant', 'placeholder.png', 3.3, 6000, 70, 1, 40,);
+  const elephant = new Animal('Elefant', 'placeholder.png', 3.3, 6000, 70, 1, 40);
   
   return (
     <div className='card'>
     <h1>{elephant.name}</h1>
-    <img alt="Elefant" height="200" width="200" 
-    src="https://picsum.photos/200/300" />
+    <img alt="Elefant" height="200" width="200" src="https://picsum.photos/200/300" />
     <table>
         <tbody>
-            <tr><td>Größe</td><td>{elephant.size} m</td></tr>
+          {Object.keys(Animal.properties).map(property => {
+            const animalProperty = Animal.properties[property];
+            return (
+              <tr key={property}>
+                <td>{animalProperty.label}</td>
+                <td>
+                  {elephant[property]}&nbsp;
+                  {animalProperty.unit}
+                </td>
+              </tr>
+            );
+          })}
+            {/* <tr><td>Größe</td><td>{elephant.size} m</td></tr>
             <tr><td>Gewicht</td><td>{elephant.weight} kg</td></tr>
             <tr><td>Alter</td><td>{elephant.age} Jahre</td></tr>
             <tr><td>Nachkommen</td><td>{elephant.offspring}</td></tr>
-            <tr><td>Geschwindigkeit</td><td>{elephant.speed} km/h</td></tr>
+            <tr><td>Geschwindigkeit</td><td>{elephant.speed} km/h</td></tr> */}
         </tbody>
     </table>
     </div>
-  )
+  );
 }
