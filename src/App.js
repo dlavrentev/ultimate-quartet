cc0d01d0e8a24d01a8ea453451121490aeee6ff5
dlavrentev <dimitrilavrentev96@gmail.com>
2022-06-27 21:27:06 +0200

    3.23 Props für die >>Card<<-Komponente

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './Style.css';
 import Card from './Components/Card';
+import Animal from './Data/Animal';
 
 function App() {
+
+  const animal = new Animal('Elefant', 'placeholder.png', 3.3, 6000, 70, 1, 40);
+
   return (
     <div className="App">
       <h1>Ultimate Quartet</h1>
-      <Card />
+      <Card animal={animal} uncovered />
     </div>
   );
 }
